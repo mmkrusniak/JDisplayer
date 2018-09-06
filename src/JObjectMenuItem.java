7dea8f2d83b485af97086858debf90f3b4b1477a
swiftm <mmkrusniak@gmail.com>
2018-09-06 19:58:50 -0400

    Restore commit.

---
+++ src/JObjectMenuItem.java
@@ -0,0 +1,14 @@
+import javax.swing.JMenuItem;
+
+public class JObjectMenuItem extends JMenuItem {
+	private ObjectBox objectbox;
+	public JObjectMenuItem(String name) {
+		super(name);
+	}
+	public ObjectBox getObjectbox() {
+		return objectbox;
+	}
+	public void setObjectbox(ObjectBox objectbox) {
+		this.objectbox = objectbox;
+	}
+}
